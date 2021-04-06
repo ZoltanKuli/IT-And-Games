899bec0d6fd2ad09dc1c20d2e2a8cb1bbcba5489
gshkd4 <gshkd4@inf.elte.hu>
2021-04-06 20:15:47 +0200

    #17 Create CI - Add A Unit Test

--- Minefield/Assets/Tests/WorldManagerTests.cs
+++ Minefield/Assets/Tests/WorldManagerTests.cs
@@ -23,6 +23,18 @@ namespace Tests {
             yield return null;
         }
 
+        [UnityTest]
+        public IEnumerator ShouldDestroyRoad() {
+            WorldManager worldManager = GetWorldManager();
+
+            worldManager.BuildNewRoad(Vector3Int.zero);
+            Assert.IsTrue(worldManager.GetFieldAtPosition(Vector3Int.zero) is Road);
+            worldManager.Destroy(Vector3Int.zero);
+            Assert.IsTrue(worldManager.GetFieldAtPosition(Vector3Int.zero) is EmptyField);
+
+            yield return null;
+        }
+
         private WorldManager GetWorldManager() { 
             return (WorldManager)SceneManager.GetSceneAt(0)
                 .GetRootGameObjects()[5].gameObject
