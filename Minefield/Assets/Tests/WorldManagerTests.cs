b4d1e58388cf9e7a32a23b3d311ad207abe4ff9c
gshkd4 <gshkd4@inf.elte.hu>
2021-04-06 18:52:35 +0200

    #18 Implement Unit Tests Base

---
+++ Minefield/Assets/Tests/WorldManagerTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Tests {
+    public class WorldManagerTest {
+
+        [SetUp]
+        public void SetUp() {
+            SceneManager.LoadScene("MineField");
+        }
+
+        [UnityTest]
+        public IEnumerator ShouldBuildRoadOnEmptyField() {
+            WorldManager worldManager = GetWorldManager();
+
+            Assert.IsTrue(worldManager.GetFieldAtPosition(Vector3Int.zero) is EmptyField);
+            worldManager.BuildNewRoad(Vector3Int.zero);
+            Assert.IsTrue(worldManager.GetFieldAtPosition(Vector3Int.zero) is Road);
+
+            yield return null;
+        }
+
+        private WorldManager GetWorldManager() { 
+            return (WorldManager)SceneManager.GetSceneAt(0)
+                .GetRootGameObjects()[5].gameObject
+                .GetComponent(typeof(WorldManager));
+        }
+    }
+}
